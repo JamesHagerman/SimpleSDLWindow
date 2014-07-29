7c2d2cf26d63feb068e96f8b4f36c66adc022554
jamis <jamis@ubuntu-13.10>
2014-07-29 02:40:43 -0700

    Updating the app with a simple keyboard handler to quit the app easily when it drops to a framebuffer on the BBB. Also adding iostream so we can see some output from this thing.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,37 @@
 #include "SDL.h"
 
+#include <iostream>
+
 SDL_Surface *screen;
 SDL_Event event;
 
+using namespace std;
+
 int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
-	screen = SDL_SetVideoMode(640, 480, 16, SDL_HWSURFACE);
+	screen = SDL_SetVideoMode(0, 0, 0, 0);
 	SDL_WM_SetCaption("Simple Window", "Simple Window");
 
 	bool done = false;
-	while(!done) {
-		while(SDL_PollEvent(&event)) {
+	while (!done) {
+		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
 				done = true;
 			}
+
+			switch (event.type) {
+				case SDL_KEYDOWN:
+					cout << "Keydown!" << endl;
+					cout << "Quitting the app!" << endl;
+					done = true;
+				case SDL_KEYUP:
+					cout << "Keyup!" << endl;
+				// case SDL_QUIT:
+				// 	done = true;
+				default:
+					break;
+			}
+
 		}
 
 		SDL_FillRect(screen, &screen->clip_rect, SDL_MapRGB(screen->format, 0, 0, 0));
